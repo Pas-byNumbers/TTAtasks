351882d1f2711f4c958e47e50395f438732de1da
Pascal R <48902056+Pas-byNumbers@users.noreply.github.com>
2023-06-26 21:52:26 +0100

    Create favourite_meal.py

---
+++ favourite_meal.py
@@ -0,0 +1,13 @@
+print("What is your favourite starter? ")
+starter = input()
+print("What is your favourite main course? ")
+main_course = input()
+print("What is your favourite dessert? ")
+dessert = input()
+print("What is your favourite drink? ")
+drink = input()
+print(f"""
+Your favourite meal is 
+{starter}, {main_course} and {dessert}
+with a glass of {drink}
+""")
