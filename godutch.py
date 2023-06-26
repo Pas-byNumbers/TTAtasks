52fde1be001a117f9ea91524e81e48cd362ce55e
Pascal R <48902056+Pas-byNumbers@users.noreply.github.com>
2023-06-26 23:00:33 +0100

    Create godutch.py

---
+++ godutch.py
@@ -0,0 +1,5 @@
+total_bill = int(input("What is the the total bill? £"))
+num_of_diners = int(input("How many diners are there? "))
+split_cost = total_bill / num_of_diners
+
+print(f"Each person should pay £{split_cost}")
